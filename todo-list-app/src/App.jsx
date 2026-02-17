ee98a020a4447eb57803b1f85f98f44f42e0e0b5
Ishan-Ostwal <ishan.ostwal@insurity.com>
2026-02-17 17:18:50 +0530

    created the frontend functionalities for the app

--- todo-list-app/src/App.jsx
+++ todo-list-app/src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
-import TodoList from './TodoList'
+import { useState } from "react";
+import "./App.css";
+import TodoList from "./TodoList";
 
 function App() {
+  const [completedTodos, setCompletedTodos] = useState([
+    { text: "Take out the garbage", isCompleted: true },
+    { text: "Complete lectures", isCompleted: true },
+  ]);
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    { text: "Make the documentation for the app", isCompleted: false },
+  ]);
+
   return (
     <>
-    <TodoList completedTodos={[]} incompleteTodos={[]}/>
+      <TodoList
+        completedTodos={completedTodos}
+        incompleteTodos={incompleteTodos}
+      />
     </>
   );
 }
 
-export default App
+export default App;
---
+++ todo-list-app/src/TodoListItem.jsx
@@ -0,0 +1,19 @@
+export default function TodoListItem({
+  todo,
+  onCompletedClicked,
+  onDeletedClicked,
+}) {
+  return (
+    <div>
+      <h3> {todo.text}</h3>
+      {todo.isCompleted && <p>Complete!</p>}
+      {todo.isCompleted ? (
+        <button onClick={() => onDeletedClicked(todo.text)}>Delete Item</button>
+      ) : (
+        <button onClick={() => onCompletedClicked(todo.text)}>
+          Mark as Complete
+        </button>
+      )}
+    </div>
+  );
+}
