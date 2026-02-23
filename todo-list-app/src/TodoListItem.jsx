782b16dee5798650df34fcbd4d53c474d6e53e1a
Ishan-Ostwal <ishan.ostwal@insurity.com>
2026-02-24 02:21:02 +0530

    completing mark as completed for todo using thunks

--- todo-list-app/src/TodoListItem.jsx
+++ todo-list-app/src/TodoListItem.jsx
@@ -1,17 +1,22 @@
-import { useDispatch } from "react-redux";
-import { markTodoAsCompleted } from "./todosSlice";
-import { deleteTodo } from "./thunks";
+import { useDispatch } from "react-redux"
+import { deleteTodo, markTodoAsCompleted } from "./thunks"
 
 export default function TodoListItem({ todo }) {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   return (
     <div>
       <h3>{todo.text}</h3>
       {todo.isCompleted && <p>Complete!</p>}
-      {todo.isCompleted
-        ? <button onClick={() => dispatch(deleteTodo(todo.id))}>Delete Item</button>
-        : <button onClick={() => dispatch(markTodoAsCompleted(todo.text))}>Mark as Completed</button>}
+      {todo.isCompleted ? (
+        <button onClick={() => dispatch(deleteTodo(todo.id))}>
+          Delete Item
+        </button>
+      ) : (
+        <button onClick={() => dispatch(markTodoAsCompleted(todo.id))}>
+          Mark as Completed
+        </button>
+      )}
     </div>
-  );
+  )
 }
